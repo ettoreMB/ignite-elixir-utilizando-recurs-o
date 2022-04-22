eb4a11a62a4d04c05cea1df380485cd24e626787
ettore <ettore.barbosa@gmail.com>
2022-04-21 22:10:05 -0300

    Challenge Done

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,14 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list) do
+    count(list, 0)
+  end
+
+  defp count([], acc) do
+    acc
+  end
+
+  defp count([head | tail], acc) do
+    acc = acc + 1
+    count(tail, acc)
   end
 end
